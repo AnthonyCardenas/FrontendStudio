8345da705d98a33ed21b6a0503560e1b514c49c2
Anthony Cardenas <ajcardenas003@gmail.com>
2026-01-29 14:56:00 -0600

    Edited About Page Paragraph

--- studio-app/src/components/AboutBanner.js
+++ studio-app/src/components/AboutBanner.js
@@ -6,27 +6,20 @@ import Dip from "../images/Grad-together-2.jpg";
 // import "../pages/Home.css";
 import "./AboutBanner.css";
 
+const AboutParagraph =
+  "We are a husband-wife duo committed to thoughtful photography and videography. Our story began when we met in a Latin dance class and we've been stepping in time ever since. Here our goal is to build your shoot in a way you can live, laugh, love in the moment and then remember those feelings every time you look at your gallery. So let's connect! We'd love to get to know you and your story.";
+
+// const OldParagraph1 =
+//   "We're a professional photographer/videographer couple, who love helping people capturing important life moments. We work with couples, families, and indie film to tell stories in your style. Brooklynnespecially loves post production to bring out the color and life of amoment.";
+// const OldParagraph2 =
+//   "My husband and I are dedicated to connecting with people and helping them find their truth in a camera lens. We capture events from weddings to family photos. Capturing people in various phases of their lives is our priveledge. From planning and moodboards to the final gallery, I keep the process simple and stress-free. If you're after timeless photos with a modern touch, let's create something beautiful together.";
+
 function AboutBanner() {
   return (
     <div className="about-wrapper">
       <div className="about-info">
         <h2>Welcome to Brooklynn Rose Studio!</h2>
-        <p>
-          We're a professional photographer/videographer couple, who love
-          helping people capturing important life moments. We work with couples,
-          families, and indie film to tell stories in your style. Brooklynn
-          especially loves post production to bring out the color and life of a
-          moment.
-        </p>
-        <p>
-          My husband and I are dedicated to connecting with people and helping
-          them find their truth in a camera lens. We capture events from
-          weddings to family photos. Capturing people in various phases of their
-          lives is our priveledge. From planning and moodboards to the final
-          gallery, I keep the process simple and stress-free. If you’re after
-          timeless photos with a modern touch, let’s create something beautiful
-          together.
-        </p>
+        <p>{AboutParagraph}</p>
       </div>
 
       <div className="photo-wrapper">
--- studio-app/src/components/TeamBanner.js
+++ studio-app/src/components/TeamBanner.js
@@ -7,6 +7,11 @@ import Anthony from "../images/AnthonyLabVert.jpg";
 import "../pages/Home.css";
 import "./TeamBanner.css";
 
+const AnthonyIntro =
+  "Hi, I'm Anthony and I love composing a shot so that you really feel the moment. From drone flying to gimbal shots, I am all hands on deck to make sure the most important moments last a lifetime. I have a background in engineering so I live by the phrase 'measure twice, cut once.' Prep work is how we make your vision a reality.";
+const BrooklynnIntro =
+  "Hi, my name is Brooklynn and I've been making videos for as long as I can remember. As a recent graduate from film school, I bring the latest industry techniques and an eye for storytelling to capture your unique love story. Color grading is where I get my dopamine hit - it's my favorite part of the process! So you can trust that your photos and videos will feel like it came straight out of a movie.";
+
 function TeamBanner() {
   return (
     <div className="team-banner">
@@ -19,18 +24,13 @@ function TeamBanner() {
           <div className="team-grid-item">
             <img className="member-photo" src={Brooklynn} alt="Brooklynn" />
             <h2 className="member-name">Brooklynn</h2>
-            <p className="member-info">
-              Hi! I'm Brooklynn and I am a media arts graduate. etc
-            </p>
+            <p className="member-info">{BrooklynnIntro}</p>
           </div>
 
           <div className="team-grid-item">
             <img className="member-photo" src={Anthony} alt="Anthony" />
             <h2 className="member-name">Anthony</h2>
-            <p className="member-info">
-              Hi! I'm Anthony and I'm a computer engineer graduate. I help my
-              wife with photography and videography.
-            </p>
+            <p className="member-info">{AnthonyIntro}</p>
           </div>
         </div>
       </div>
